2e6a8778f45ae264ba469378516b4621666eca1b
livgimenes <77754389+livgimenes@users.noreply.github.com>
2023-05-02 12:54:27 -0400

    Adding the current available data

--- data/fetch.py
+++ data/fetch.py
@@ -3,6 +3,10 @@ import requests.auth
 import pandas as pd
 import re
 import nltk
+import praw
+import asyncio
+import datetime
+
 
 
 # You might have to do pip install emoji
@@ -13,82 +17,124 @@ import emoji
 #### Requesting a token ####
 # NOTE: This requests a token, which should work for more than one call, for security reasons the password is not on display, but if you need to request ask Livia
 
-# username = "project-user"
-# password = ""
-# app_client_id = "dyczxLp57fVfQrBb4epVQA"
-# app_secret = "CEpQfSvjR0aJ2-0deD7xyEvIv4uF5A"
+username = "project-user"
+password = ""
+app_client_id = "dyczxLp57fVfQrBb4epVQA"
+app_secret = "CEpQfSvjR0aJ2-0deD7xyEvIv4uF5A"
 
-# client_auth = requests.auth.HTTPBasicAuth(app_client_id, app_secret)
-# post_data = {"grant_type": "password", "username": username, "password": password}
-# headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+client_auth = requests.auth.HTTPBasicAuth(app_client_id, app_secret)
+post_data = {"grant_type": "password", "username": username, "password": password}
+headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
 
 # response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
 # TOKEN = response.json()['access_token']
 
-
 #### Response received ####
 
-TOKEN = '26658734656586-7GRT_v-Ehz000K0xuX3tEupSWSY9ig'
-print(TOKEN)
+
+###### Getting the main corpus of data #####
+
+reddit = praw.Reddit(
+    client_id=app_client_id,
+    client_secret=app_secret,
+    user_agent=username,
+)
+
+# Set the subreddit and keyword
+subrdt_df = pd.read_csv("data/subreddits.csv")
+subreddits_list = list(subrdt_df["subreddit_tag"])
+
+#### Generate glossary lists
+nasa_glossary = pd.read_csv("data/glossaries/nasa_glossary.csv")
+davis_glossary = pd.read_csv("data/glossaries/davis_glossary.csv")
+epa_glossary = pd.read_csv("data/glossaries/epa_glossary.csv")
+glossaries = [nasa_glossary, davis_glossary, epa_glossary]
 
 
+def generate_glossary_types(glossary):
+    clean = [keyword.lower() for keyword in glossary]
+    return glossary, clean
+
+
+def get_posts_from_glossaries(glossary):
+    unclean_keywords, clean_keywords = generate_glossary_types(glossary)
+    matching_posts = []
+    for subred in subreddits_list:
+        subreddit = reddit.subreddit(subred)
+        found_count = 0
+        for post in subreddit.search(query=' OR '.join(unclean_keywords + clean_keywords), time_filter='all'):
+            print(post.title)
+            found_count += 1
+            matching_posts.append([post.id, post.subreddit.display_name, post.title, post.author.name, post.url, post.selftext, post.score, post.num_comments, post.created_utc])
+
+    # Convert matching_posts list into a pandas dataframe
+    cols = ['id', 'subreddit', 'title', 'author', 'url', 'selftext', 'score', 'num_comments', 'created_utc']
+    matching_posts_df = pd.DataFrame(matching_posts, columns=cols)
+    
+    return matching_posts_df
+
+
+
+# epa_data = get_posts_from_glossaries(epa_glossary)
+# print(epa_data.head())
+# print(epa_data.shape)
+# print(epa_data.shape)
+# nasa_data = get_posts_from_glossaries(nasa_glossary)
+# print(nasa_data.head())
+# print(nasa_data.shape)
+# davis_data = get_posts_from_glossaries(davis_glossary)
+# print(davis_data.head())
+# print(davis_data.shape)
+
+# all_data = pd.concat([epa_data, nasa_data, davis_data])
+
+# # get only the unique rows that have a unique entry for the id column
+
+# all_data = all_data.drop_duplicates(subset=['id'])
+
+# print(all_data.shape)
+# print(all_data.head())
+
+# # save the data to a csv file
+
+# all_data.to_csv("data/raw_data/raw_politcal_posts_two.csv", index=False)
+
+
+
+##### Getting mock truth and fake labels #####
+
+def get_posts_from_one(glossary,subred):
+    unclean_keywords, clean_keywords = generate_glossary_types(glossary)
+    matching_posts = []
+    subreddit = reddit.subreddit(subred)
+    found_count = 0
+    for post in subreddit.search(query=' OR '.join(unclean_keywords + clean_keywords), time_filter='all'):
+        print(post.title)
+        found_count += 1
+        matching_posts.append([post.id, post.subreddit.display_name, post.title, post.author.name, post.url, post.selftext, post.score, post.num_comments, post.created_utc])
+
+    # Convert matching_posts list into a pandas dataframe
+    cols = ['id', 'subreddit', 'title', 'author', 'url', 'selftext', 'score', 'num_comments', 'created_utc']
+    matching_posts_df = pd.DataFrame(matching_posts, columns=cols)
+    
+    return matching_posts_df
+
+skeptics_data = get_posts_from_one(epa_glossary,'climateskeptics')
+print(skeptics_data.head())
+print(skeptics_data.shape)
+
+
+scientists_data = get_posts_from_one(epa_glossary,'climate_science')
+print(scientists_data.head())
+print(scientists_data.shape)
+
+
+#save 
+skeptics_data.to_csv("data/raw_data/skeptics_data.csv", index=False)
+scientists_data.to_csv("data/raw_data/scientists_data.csv", index=False)
 #### Requesting data ####
 
 
-###### This section has the code that just gets the posts for subreddits where key words don't need to be searched for ####
-headers = {"Authorization": "bearer 26658734656586-2gMR6jRsCRQX8Qm_bNzvpb8wcjBNuQ", "User-Agent": "'your bot 0.1"}
-
-#### First just working with all the post climate skeptics data ####
-url = f"https://api.pushshift.io/reddit/submission/search/?subreddit=climateskeptics"
-
-# Create an empty list to hold all the posts
-
-
-def get_subreddit_posts(url):
-    
-    params = {"size": 1000}
-    response = requests.get(url, params=params)
-    if response.status_code != 200:
-        #throw an error if the request fails
-        raise Exception(response.status_code, response.text)
-    data = response.json()["data"]
-
-    all_posts = []
-    for post in data:
-            post_data = {
-                "title": post["title"],
-                "author": post["author"],
-                "body": post["selftext"],
-                "created_utc": post["created_utc"],
-                "score": post["score"],
-                "num_comments": post["num_comments"],
-                "subreddit": post["subreddit"]
-            }
-            all_posts.append(post_data)
-    df = pd.DataFrame(all_posts)
-    print(df.head())
-    return df
-
-subreddit = ["climateskeptics"]
-#save to csv
-def get_all_of_subreddit(subreddit_lst):
-    all= []
-    for subredit in subreddit_lst:
-        url = f"https://api.pushshift.io/reddit/submission/search/?subreddit={subredit}"
-        df = get_subreddit_posts(url)
-        all.append(df)
-    all_dfs = pd.concat(all)
-    all_dfs.to_csv("data/unclean_subreddits.csv", index=False)
-    return all_dfs
-
-result = get_all_of_subreddit(subreddit)
-
-print(result.head())
-    
-   
-
-
-### Expand the code for getting the data with the search for specific subreddits #
 
 #### Cleaning the data ####'
 
@@ -122,6 +168,5 @@ def clean_body(df):
     #saving the clean data
     return df
 
-body = clean_body(result)
-body.to_csv("data/clean_subreddits.csv", index=False)
+
 
