2dbe2d144c03730578deb9a32f55f8a0e0f3f9c9
crivera008 <77813121+crivera008@users.noreply.github.com>
2023-05-10 22:16:02 -0400

    Update data_updates.py

--- model_data_modification/data_updates.py
+++ model_data_modification/data_updates.py
@@ -11,16 +11,17 @@ def make_binary(label):
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     data = pd.read_csv(f)
-    data['claim'] = data['claim'].apply(make_binary)
+    data['claim'] = data['claim'].apply(lambda x: x[0])
     data.to_csv('cleaned_data/' + filename, index=False)
 
 
 ## note that the code below was continually modified to test different distributions of data
-for filename in os.listdir('cleaned_data'):
-    data = pd.read_csv('cleaned_data/' + filename)
-    misinfo = data[data['claim'] != 0]
-    info = data[data['claim'] == 0].sample(int(len(misinfo)/5)).reset_index(drop=True)
-    smashed = pd.concat([info, misinfo])
-    smashed = smashed.sample(frac=1).reset_index(drop=True)
-    smashed.to_csv('cleaned_data/' + filename, index=False)
-    print(len(smashed))
+data = pd.read_csv('cleaned_data/training.csv')
+misinfo = data[data['claim'] != 0]
+print(len(misinfo))
+info = data[data['claim'] == 0].sample(int(len(misinfo)/5)).reset_index(drop=True)
+smashed = pd.concat([info, misinfo])
+smashed = smashed.sample(frac=1).reset_index(drop=True)
+smashed.to_csv('cleaned_data/training2.csv', index=False)
+print(len(smashed))
+print(len(misinfo)/5)
